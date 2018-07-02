4de6938b1b0296fea43a52e86f1b6282bf7aa4eb
Betsabeth Torres <kyaalena@railsgirls.gnu.ve>
2018-07-02 09:41:33 -0400

    add gem decidim verifications

---
+++ decidim-verifications/lib/decidim/verifications/version.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module Decidim
+  # This holds the decidim-verifications version.
+  module Verifications
+    def self.version
+      "0.12.1"
+    end
+  end
+end
